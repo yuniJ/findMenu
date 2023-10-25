8565e7d76f6c0d438cdcfb863b3e54beffce8801
alandscape <alandscape@neowiz.com>
2023-10-25 17:26:54 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://yuniJ.github.io/findMenu/",
+  base: "/",
   integrations: [react()],
 });
