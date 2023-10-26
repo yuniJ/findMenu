f33454671db049028e6e5e2d9e5f285e99a22b28
alandscape <alandscape@neowiz.com>
2023-10-26 13:11:04 +0900

    -

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yuniJ.github.io/findMenu/",
-  base: "/",
+  site: "https://yuniJ.github.io",
+  base: "/findMenu/",
   build: {
     assets: "astro",
-    image: {
-      // Example: Allow remote image optimization from a single domain
-      domains: ["https://yunij.github.io/findMenu/"],
-    },
+  },
+  image: {
+    domains: ["astro.build"],
   },
   integrations: [react()],
 });
